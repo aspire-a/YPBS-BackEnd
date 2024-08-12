9436715872f5634a8973802c460a22bb2b5aa662
Serkan YILMAZ <serkan.yilmaz@tubitak.gov.tr>
2024-08-12 09:40:05 +0300

    YPBS-313 Authority populated in import.sgl

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -158,3 +158,40 @@ INSERT INTO Kurumsal_Proje (kurumsal_id, proje_id, gorev)VALUES (31,1,'Mentör')
 INSERT INTO Kurumsal_Proje (kurumsal_id, proje_id, gorev)VALUES (32,1,'Full-Stack Developer');
 INSERT INTO Kurumsal_Proje (kurumsal_id, proje_id, gorev)VALUES (32,12,'Project Lead');
 INSERT INTO Kurumsal_Proje (kurumsal_id, proje_id, gorev)VALUES (33,16,'Backend Developer');
+
+INSERT INTO Authority (authority) VALUES ( 'ROLE_USER' );
+INSERT INTO Authority (authority) VALUES ( 'ROLE_ADMIN' );
+
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 1, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 2, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 3, 2 ); -- serkan.yilmaz 123 admin
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 4, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 5, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 6, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 7, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 8, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 9, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 10, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 11, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 12, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 13, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 14, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 15, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 16, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 17, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 18, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 19, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 20, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 21, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 22, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 23, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 24, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 25, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 26, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 27, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 28, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 29, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 30, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 31, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 32, 1 );
+INSERT INTO User_Authority (user_id, authority_id) VALUES ( 33, 1 );
